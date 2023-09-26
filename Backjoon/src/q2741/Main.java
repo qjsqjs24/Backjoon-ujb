8546abc2add8b4c4ed68ddc012fafe31a01a60d0
21-manic-020g <21-manic-020g@21-manic-020>
2023-09-26 15:37:43 +0900

    풀이

---
+++ Backjoon/src/q2741/Main.java
@@ -0,0 +1,18 @@
+package q2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for(int i=1; i<=a; i++) {
+			System.out.println(i);
+		}
+		
+		sc.close();
+
+	}
+
+}
